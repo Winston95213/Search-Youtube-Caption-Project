feeafba2c83b6f33ee9851b6c289e4c139ffc269
Winston95213 <59035990+Winston95213@users.noreply.github.com>
2022-06-20 16:44:43 +0800

    Save video list, download caption -> Bug Fixed

--- Search_Youtube_Caption/pipeline/Steps/download_captions.py
+++ Search_Youtube_Caption/pipeline/Steps/download_captions.py
@@ -1,4 +1,5 @@
-import os, time
+import os
+import time
 
 from pytube import YouTube
 
@@ -7,22 +8,22 @@ from .step import StepException
 
 
 class DownloadCaptions(Step):
-
     def process(self, data, inputs, utils):
-        existing_caption_counter = 0
         for url in data:
-            print("downloading caption for", url)
             if utils.caption_file_exists(url):
-                existing_caption_counter += 1
-                print(f'found {existing_caption_counter} existing caption file')
+                print('found existing caption file')
                 continue
+
             try:
                 source = YouTube(url)
-                en_caption = source.captions.get_by_language_code('a.en')
-                en_caption_covert_to_srt = (en_caption.generate_srt_captions())
+                en_caption = source.captions['a.en']
+                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
             except (KeyError, AttributeError):
-                print("KeyError when download caption for", url)
+                print('Error when downloading caption for: ', url)
+                continue
 
-            text_file = open(utils.get_video_id_from_url(url), 'w', encoding='utf-8')
-            text_file.write(en_caption_covert_to_srt)
+            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file.write(en_caption_convert_to_srt)
             text_file.close()
+
+        return data
--- Search_Youtube_Caption/pipeline/Steps/video_list.py
+++ Search_Youtube_Caption/pipeline/Steps/video_list.py
@@ -31,7 +31,7 @@ class GetVideoList(Step):
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
+        first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY2,
                                                                                                             channel_id)
         print(first_url)
 
