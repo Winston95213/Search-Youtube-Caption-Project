a551b058fd4aef1c513e87b00dac9d2b925307fc
Winston95213 <59035990+Winston95213@users.noreply.github.com>
2022-06-26 20:57:06 +0800

    Search Key Word, Download Video, Bug Fixed

--- Search_Youtube_Caption/pipeline/Steps/read_caption.py
+++ Search_Youtube_Caption/pipeline/Steps/read_caption.py
@@ -1,33 +1,27 @@
-import os
-
-from pprint import pprint
-
 from .step import Step
-from Search_Youtube_Caption.setting import CAPTION_DIR
 
 
 class ReadCaption(Step):
     def process(self, data, inputs, utils):
-        data = {}
-        for caption_file in os.listdir(CAPTION_DIR):
+        for yt in data:
+            if not utils.caption_file_exists(yt):
+                continue
+
             captions = {}
-            with open(os.path.join(CAPTION_DIR, caption_file), 'r') as f:
+            with open(yt.caption_filepath, 'r') as f:
                 time_line = False
-                caption = None
                 time = None
+                caption = None
                 for line in f:
                     line = line.strip()
                     if '-->' in line:
                         time_line = True
                         time = line
                         continue
-
                     if time_line:
                         caption = line
                         captions[caption] = time
                         time_line = False
+            yt.captions = captions
 
-            data[caption_file] = captions
-
-        pprint(data)
         return data
